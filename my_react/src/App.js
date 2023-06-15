3f383089fa1a1849f4e3a4e0696025d5b9c7c9aa
Suhas <suhasps05@gmail.com>
2023-06-16 03:30:04 +0530

    Profile route removed now only one route /

--- my_react/src/App.js
+++ my_react/src/App.js
@@ -7,7 +7,7 @@ function App(){
   return (
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
+        {/* <Route path="/profile" element={<Profile />} /> */}
         {/* <Route path="/register" element={<Register />} /> */}
       </Routes>
       
